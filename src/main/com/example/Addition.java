871edbe3dd52889f3af0fe57f212d678ec758cd0
G-MaheshReddy <166423657+G-MaheshReddy@users.noreply.github.com>
2024-11-27 12:14:46 +0530

    Create Addition.java

---
+++ src/main/com/example/Addition.java
@@ -0,0 +1,10 @@
+public class Addition {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        Addition addition = new Addition();
+        System.out.println("Sum: " + addition.add(5, 10));
+    }
+}
